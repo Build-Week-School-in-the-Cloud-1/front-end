9c4b86495c4b430af38bdae6f2dba06f3b938ad4
Hamida Sharifova <hamida@Hamidas-MacBook-Pro.local>
2020-08-21 01:11:03 -0700

    Login component initial changes

--- school-in-the-cloud/src/App.js
+++ school-in-the-cloud/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Login from './components/Forms/LoginForm'
 
 function App() {
   return (
     <div className="App">
-      
+      <Login/>
     </div>
   );
 }
